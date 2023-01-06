2d5943163a4290a6fd26763037d9f73f26bdb3ab
Ty Reid <trawx11@gmail.com>
2023-01-06 06:57:33 -0500

    term default value added

--- src/api.js
+++ src/api.js
@@ -1,14 +1,11 @@
 import axios from 'axios';
 
-const searchImages = async (term) => {
-    const response = await axios.get('https://api.unsplash.com/search/photos', {
-        headers: {
-            Authorization: 'Client-ID dPoZL5Xc0BTClnQx1vRM0kFuud4KSimCK3r8YqRFaho',
-        }, 
-        params: {
+const searchImages = async (term = 'bitcoin') => {
+    const response = await axios.get('https://api.unsplash.com/photos/?client_id=Iu6vPciw46vieGuT_aKcQQJ_Jjrj5slkDpKHFRJKSI0', { 
+        
             query: term,
         },
-    });
+    );
 
     console.log(response);
     return response.data.results;
